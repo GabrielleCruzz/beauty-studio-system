12cb1f21eaf313cbd0a8be168bdc83bc138e5a7c
GabrielleCruzz <gabriellefcruz207@gmail.com>
2026-05-22 16:29:53 -0300

    feat(schedule): add new appointment modal

--- script.js
+++ script.js
@@ -37,3 +37,72 @@ function mostrarData() {
 }
 
 mostrarData();
+
+
+// Modal
+
+const overlay = document.querySelector(".overlay");
+const abrirModal = document.getElementById("abrir-modal");
+const fecharModal = document.getElementById("fechar-modal");
+const cancelar = document.querySelector(".cancelar");
+const form = document.querySelector(".form-agendamento")
+const selectProcedimento = document.getElementById("procedimento");
+const listaProcedimentos = document.getElementById("lista-procedimentos");
+
+abrirModal.addEventListener("click", () => {
+    overlay.classList.add("abrir");
+    document.body.style.overflow = "hidden";
+});
+
+fecharModal.addEventListener("click", () => {
+    overlay.classList.remove("abrir");
+    document.body.style.overflow = "auto";
+    form.reset();
+});
+
+cancelar.addEventListener("click", () => {
+    overlay.classList.remove("abrir");
+    document.body.style.overflow = "auto"
+    form.reset();
+});
+
+
+// Selecionar vários procedimentos
+
+let procedimentosSelecionados = [];
+
+selectProcedimento.addEventListener("change", () => {
+    const procedimento = selectProcedimento.value;
+
+    if (!procedimento) return;
+
+    // evita repetir o mesmo procedimento
+    if (procedimentosSelecionados.includes(procedimento)) {
+        selectProcedimento.value = "";
+        return;
+    }
+
+    procedimentosSelecionados.push(procedimento);
+    criarItemProcedimento(procedimento);
+    selectProcedimento.value = "";
+});
+
+function criarItemProcedimento(nome) {
+    const item = document.createElement("div");
+    item.classList.add("item-procedimento");
+
+    item.innerHTML = `
+        <span>${nome}</span>
+        <button class="btn-remover">✕</button>
+    `;
+
+    const botaoRemover = item.querySelector(".btn-remover");
+    botaoRemover.addEventListener("click", () => {
+        procedimentosSelecionados =
+            procedimentosSelecionados.filter(p => p !== nome);
+
+        item.remove();
+    });
+
+    listaProcedimentos.appendChild(item);
+}
