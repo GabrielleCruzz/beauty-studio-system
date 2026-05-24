59a487984050712b9cbf0e49700d28c6869ab265
GabrielleCruzz <gabriellefcruz207@gmail.com>
2026-05-24 12:19:46 -0300

    feat(finance): add monthly appointments line chart

--- js/financeiro.js
+++ js/financeiro.js
@@ -56,6 +56,8 @@ new Chart(ctx, {
 
     options: {
         responsive: true,
+        maintainAspectRatio: false,
+
         plugins: {
             legend: {
                 display: false
@@ -65,6 +67,66 @@ new Chart(ctx, {
                 callbacks: {
                     label: function(context) {
                         return `R$ ${context.raw}`;
+                    }
+                }
+            }
+        }
+    }
+});
+
+
+// Gráfico total de atendimento
+
+const ctxAtendimentos =
+    document.getElementById("graficoAtendimentos");
+
+new Chart(ctxAtendimentos, {
+    type: "line",
+
+    data: {
+        labels: [
+            "Jan",
+            "Fev",
+            "Mar",
+            "Abr",
+            "Mai",
+            "Jun"
+        ],
+
+        datasets: [{
+            label: "Atendimentos",
+
+            data: [
+                45,
+                62,
+                39,
+                80,
+                120,
+                74
+            ],
+
+            borderColor: "#d79bfd",
+            backgroundColor: "rgba(205, 151, 241, 0.2)",
+            tension: 0.4,
+            fill: true,
+            pointRadius: 5,
+            pointHoverRadius: 8
+        }]
+    },
+
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+
+        plugins: {
+            legend: {
+                display: false
+            },
+
+            tooltip: {
+                callbacks: {
+                    label: function(context) {
+                        return `${context.raw} atendimentos`;
                     }
                 }
             }
