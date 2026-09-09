efcd77de7f368abf9709a72ff551a86dbb6fce92
GabrielleCruzz <gabriellefcruz207@gmail.com>
2026-09-09 19:18:39 -0300

    fix(menu): correct component path

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,18 @@
-fetch("../components/menu.html")
+const caminhoMenu = new URL("../components/menu.html", document.currentScript.src);
+
+fetch(caminhoMenu)
     .then(response => response.text())
     .then(data => {
 
         document.getElementById("menu").innerHTML = data;
+
+        const links = document.querySelectorAll(".menu a");
+        const estaNasPages = window.location.pathname.includes("/pages/");
+
+        links[0].href = estaNasPages ? "../index.html" : "./index.html";
+        links[1].href = estaNasPages ? "./schedule.html" : "./pages/schedule.html";
+        links[2].href = estaNasPages ? "./finance.html" : "./pages/finance.html";
+        links[3].href = estaNasPages ? "./clients.html" : "./pages/clients.html";
 
         ativarMenu();
 
@@ -10,13 +20,15 @@ fetch("../components/menu.html")
 
 function ativarMenu() {
 
-    const paginaAtual = window.location.href;
+    const paginaAtual = window.location.pathname;
 
     const links = document.querySelectorAll(".menu a");
 
     links.forEach(link => {
 
-        if (paginaAtual.includes(link.getAttribute("href"))) {
+        const destino = new URL(link.href);
+
+        if (destino.pathname === paginaAtual) {
             link.classList.add("active");
         }
 
